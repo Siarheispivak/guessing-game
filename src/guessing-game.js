1916e859452caa791dd478189aafcb735f84b9b7
Siarheispivak <siarheispivak2@gmail.com>
2022-08-15 01:54:52 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+	constructor() {
+        
     }
 
-    guess() {
+	setRange(min, max) {
+		this.min = min;
+		this.max = max;
+		
+	}
 
+	guess() {
+		this.mid = Math.round((this.max + this.min) / 2);
+        return this.mid;
+	}
+
+	lower() {
+        this.max = this.mid;
     }
 
-    lower() {
-
-    }
-
-    greater() {
-
+	greater() {
+        this.min = this.mid;
     }
 }
 
